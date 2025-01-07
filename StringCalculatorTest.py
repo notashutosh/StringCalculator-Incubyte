8ef9b7892b436306ace18fe9c991627f6e554715
Ashutosh Mohan <ashutoshmohan@outlook.in>
2025-01-07 08:02:51 +0530

    Adds test for non-empty string input

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -6,5 +6,8 @@ class StringCalculatorTest(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(sumOfString(""), 0)
 
+    def test_nonempty(self):
+        self.assertEqual(sumOfString("1"), 1)
+
 
 unittest.main()
