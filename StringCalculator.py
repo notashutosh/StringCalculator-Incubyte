21745e971f8f973b462d6033873d51e86596c004
Ashutosh Mohan <ashutoshmohan@outlook.in>
2025-01-07 08:28:16 +0530

    Implements processing of multiple delimiters

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,18 +1,38 @@
+import re
+
+
+def splitString(str, delimiters=[',']):
+    """
+    Splits the input string using the specified delimiters.
+
+    Args:
+        str (str): The input string to be split.
+        delimiters (list, optional): A list of delimiter strings to split the input string. Defaults to [','].
+
+    Returns:
+        list: A list of substrings obtained by splitting the input string using the specified delimiters.
+
+    Notes:
+    - a regular expression is created by combining the delimiters using the '|' operator after escaping them. This is then used to split the input string. 
+    """
+    return re.split(f'{"|".join(map(re.escape, delimiters))}', str)
+
+
 def sumOfString(input):
     if len(input) == 0:
         return 0
     sum = 0
-    delimiter = ','
+    delimiters = [',']
 
     if input.startswith("//"):
         delimiterString, numberString = input.split("\n")
-        delimiter = delimiterString[2:]
+        delimiters = delimiterString[2:].strip("[]").split("][")
     else:
         numberString = input
 
     negative_numbers = []
     negatives_present = False
-    for number in numberString.split(delimiter):
+    for number in splitString(numberString, delimiters):
         n = int(number)
         if n >= 0:
             if n > 1000:
